7d367eb0c88a286e590b150536bfacc8e1b519c4
Mayis Qarayev <mayisqarayev75@gmail.com>
2026-05-06 17:03:58 +0400

    feat(auth-service): add role repository

--- services/auth-service/src/main/java/com/mayis/auth_service/repository/RoleRepository.java
+++ services/auth-service/src/main/java/com/mayis/auth_service/repository/RoleRepository.java
@@ -1,4 +1,15 @@
 package com.mayis.auth_service.repository;
 
-public interface RoleRepository {
+import com.mayis.auth_service.model.entity.Role;
+import com.mayis.auth_service.model.enums.RoleName;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role, UUID> {
+
+    Optional<Role> findByName(RoleName name);
 }
