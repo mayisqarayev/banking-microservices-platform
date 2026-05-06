c941d60f934698e91ef36a1a41366848b4400965
Mayis Qarayev <mayisqarayev75@gmail.com>
2026-05-06 17:03:58 +0400

    feat(auth-service): add refresh token repository

--- services/auth-service/src/main/java/com/mayis/auth_service/repository/RefreshTokenRepository.java
+++ services/auth-service/src/main/java/com/mayis/auth_service/repository/RefreshTokenRepository.java
@@ -1,4 +1,14 @@
 package com.mayis.auth_service.repository;
 
-public interface RefreshTokenRepository {
+import com.mayis.auth_service.model.entity.RefreshToken;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+@Repository
+public interface RefreshTokenRepository extends JpaRepository<RefreshToken, UUID> {
+
+    Optional<RefreshToken> findByTokenHash(String tokenHash);
 }
