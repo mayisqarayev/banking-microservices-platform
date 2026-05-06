a08c466b8cbc4d6aaf594b1fc19adbb2e930df35
Mayis Qarayev <mayisqarayev75@gmail.com>
2026-05-06 16:00:31 +0400

    feat(auth-service): add Role entity

--- services/auth-service/src/main/java/com/mayis/auth_service/model/entity/Role.java
+++ services/auth-service/src/main/java/com/mayis/auth_service/model/entity/Role.java
@@ -1,4 +1,25 @@
 package com.mayis.auth_service.model.entity;
 
-public class Role {
+import com.mayis.auth_service.model.base.SoftDeletableEntity;
+import com.mayis.auth_service.model.enums.RoleName;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.UUID;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "roles")
+public class Role extends SoftDeletableEntity {
+
+    @Id
+    private UUID id;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false, unique = true)
+    private RoleName name;
+
+    private String description;
 }
