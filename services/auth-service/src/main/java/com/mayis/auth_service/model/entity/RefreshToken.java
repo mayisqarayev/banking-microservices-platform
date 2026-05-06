f43dd61217daf317e4f29854241a38fa7279b6ed
Mayis Qarayev <mayisqarayev75@gmail.com>
2026-05-06 17:03:57 +0400

    feat(auth-service): add refresh token entity

--- services/auth-service/src/main/java/com/mayis/auth_service/model/entity/RefreshToken.java
+++ services/auth-service/src/main/java/com/mayis/auth_service/model/entity/RefreshToken.java
@@ -1,4 +1,46 @@
 package com.mayis.auth_service.model.entity;
 
-public class RefreshToken {
+import com.mayis.auth_service.model.base.BaseEntity;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "refresh_tokens")
+public class RefreshToken extends BaseEntity {
+
+    @Id
+    private UUID id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @Column(nullable = false)
+    private String tokenHash;
+
+    @Column(nullable = false)
+    private LocalDateTime expiresAt;
+
+    @Column(nullable = false)
+    private boolean revoked = false;
+
+    @PrePersist
+    public void prePersist() {
+        if (id == null) {
+            id = UUID.randomUUID();
+        }
+
+        createdAt = LocalDateTime.now();
+    }
+
+    @PreUpdate
+    public void preUpdate() {
+        updatedAt = LocalDateTime.now();
+    }
 }
