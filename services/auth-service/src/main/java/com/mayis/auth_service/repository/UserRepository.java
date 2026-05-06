c359736fc418a1c250a7e56d20e3d671b35f6afc
Mayis Qarayev <mayisqarayev75@gmail.com>
2026-05-06 17:03:58 +0400

    feat(auth-service): add user repository

--- services/auth-service/src/main/java/com/mayis/auth_service/repository/UserRepository.java
+++ services/auth-service/src/main/java/com/mayis/auth_service/repository/UserRepository.java
@@ -1,4 +1,20 @@
 package com.mayis.auth_service.repository;
 
-public interface UserRepository {
+import com.mayis.auth_service.model.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, UUID> {
+
+    Optional<User> findByUsernameAndDeletedFalse(String username);
+
+    Optional<User> findByEmailAndDeletedFalse(String email);
+
+    boolean existsByUsername(String username);
+
+    boolean existsByEmail(String email);
 }
