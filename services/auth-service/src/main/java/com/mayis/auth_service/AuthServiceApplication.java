3d813b3b5d2500b42e6c63bebd8339f71adedbda
Mayis Qarayev <mayisqarayev75@gmail.com>
2026-05-07 10:49:54 +0400

    refactor(auth-service): enable jwt configuration setup

--- services/auth-service/src/main/java/com/mayis/auth_service/AuthServiceApplication.java
+++ services/auth-service/src/main/java/com/mayis/auth_service/AuthServiceApplication.java
@@ -1,9 +1,12 @@
 package com.mayis.auth_service;
 
+import com.mayis.auth_service.config.properties.JwtProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties(JwtProperties.class)
 public class AuthServiceApplication {
 
 	public static void main(String[] args) {
