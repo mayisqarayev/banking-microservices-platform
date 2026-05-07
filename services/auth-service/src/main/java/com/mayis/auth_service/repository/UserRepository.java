018d8015eb6ae21d1caf7fe0f701f85e3bce4db0
Mayis Qarayev <mayisqarayev75@gmail.com>
2026-05-07 12:48:45 +0400

    feat(repository): add entity graph for user roles loading

--- services/auth-service/src/main/java/com/mayis/auth_service/repository/UserRepository.java
+++ services/auth-service/src/main/java/com/mayis/auth_service/repository/UserRepository.java
@@ -1,6 +1,7 @@
 package com.mayis.auth_service.repository;
 
 import com.mayis.auth_service.model.entity.User;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -10,8 +11,10 @@ import java.util.UUID;
 @Repository
 public interface UserRepository extends JpaRepository<User, UUID> {
 
+    @EntityGraph(attributePaths = {"roles", "roles.role"})
     Optional<User> findByUsernameAndDeletedFalse(String username);
 
+    @EntityGraph(attributePaths = {"roles", "roles.role"})
     Optional<User> findByEmailAndDeletedFalse(String email);
 
     boolean existsByUsername(String username);
