8dc6ae5350b968d6adfea89ee122eccb217d7983
Mayis Qarayev <mayisqarayev75@gmail.com>
2026-05-07 10:50:08 +0400

    feat(auth-service): implement custom user details service

--- services/auth-service/src/main/java/com/mayis/auth_service/security/CustomUserDetailsService.java
+++ services/auth-service/src/main/java/com/mayis/auth_service/security/CustomUserDetailsService.java
@@ -1,4 +1,28 @@
 package com.mayis.auth_service.security;
 
-public class CustomUserDetailsService {
+import com.mayis.auth_service.model.entity.User;
+import com.mayis.auth_service.repository.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    public CustomUserDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return userRepository.findByUsernameAndDeletedFalse(username)
+                .orElseThrow(() ->
+                        new UsernameNotFoundException(
+                                "User not found with username: " + username
+                        )
+                );
+    }
 }
