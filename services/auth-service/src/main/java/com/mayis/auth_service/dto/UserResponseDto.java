969ee24b3dc59c2a400ba98844107590be5703a6
Mayis Qarayev <mayisqarayev75@gmail.com>
2026-05-07 10:55:32 +0400

    feat(auth-service): add UserResponseDto for authenticated user responses

--- services/auth-service/src/main/java/com/mayis/auth_service/dto/UserResponseDto.java
+++ services/auth-service/src/main/java/com/mayis/auth_service/dto/UserResponseDto.java
@@ -1,4 +1,13 @@
 package com.mayis.auth_service.dto;
 
-public record UserResponseDto() {
+import java.util.UUID;
+
+public record UserResponseDto(
+        UUID id,
+        String username,
+        String email,
+        String firstName,
+        String lastName,
+        String status
+) {
 }
