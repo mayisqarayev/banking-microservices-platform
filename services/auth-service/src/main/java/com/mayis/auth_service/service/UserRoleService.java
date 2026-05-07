62ddb5f6a4a3c61bce8c75ed33291966e5620312
Mayis Qarayev <mayisqarayev75@gmail.com>
2026-05-07 12:49:04 +0400

    feat(user-role): add user role assignment service

--- services/auth-service/src/main/java/com/mayis/auth_service/service/UserRoleService.java
+++ services/auth-service/src/main/java/com/mayis/auth_service/service/UserRoleService.java
@@ -1,4 +1,28 @@
 package com.mayis.auth_service.service;
 
+import com.mayis.auth_service.dto.CreateUserRoleRequestDto;
+import com.mayis.auth_service.model.entity.UserRole;
+import com.mayis.auth_service.repository.UserRoleRepository;
+import org.springframework.stereotype.Service;
+
+@Service
 public class UserRoleService {
+
+    private final UserRoleRepository repository;
+    private final UserService userService;
+    private final RoleService roleService;
+
+    public UserRoleService(UserRoleRepository repository, UserService userService, RoleService roleService) {
+        this.repository = repository;
+        this.userService = userService;
+        this.roleService = roleService;
+    }
+
+    public void create(CreateUserRoleRequestDto requestDto) {
+        UserRole userRole = new UserRole();
+        userRole.setUser(userService.getUserById(requestDto.userId()));
+        userRole.setRole(roleService.getRoleById(requestDto.roleId()));
+
+        repository.save(userRole);
+    }
 }
