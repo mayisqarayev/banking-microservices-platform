a6b3aeb9b172e49d19df9ec1d5199d331e11e8bd
Mayis Qarayev <mayisqarayev75@gmail.com>
2026-05-07 12:47:48 +0400

    feat(user): implement user management controller

--- services/auth-service/src/main/java/com/mayis/auth_service/controller/UserController.java
+++ services/auth-service/src/main/java/com/mayis/auth_service/controller/UserController.java
@@ -1,4 +1,26 @@
 package com.mayis.auth_service.controller;
 
+import com.mayis.auth_service.dto.UserResponseDto;
+import com.mayis.auth_service.model.entity.User;
+import com.mayis.auth_service.service.UserService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/user")
 public class UserController {
+
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping
+    public List<UserResponseDto> getAll() {
+        return userService.getAll();
+    }
 }
