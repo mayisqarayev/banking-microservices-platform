3885094ab1b611f5b1320a39d62efb20c917b241
Mayis Qarayev <mayisqarayev75@gmail.com>
2026-05-07 10:55:28 +0400

    feat(auth-service): add RegisterRequestDto for user registration flow

--- services/auth-service/src/main/java/com/mayis/auth_service/dto/RegisterRequestDto.java
+++ services/auth-service/src/main/java/com/mayis/auth_service/dto/RegisterRequestDto.java
@@ -1,4 +1,29 @@
 package com.mayis.auth_service.dto;
 
-public record RegisterRequest() {
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+public record RegisterRequestDto(
+        @NotBlank
+        @Size(min = 3, max = 100)
+        String username,
+
+        @NotBlank
+        @Email
+        @Size(max = 150)
+        String email,
+
+        @NotBlank
+        @Size(min = 8, max = 100)
+        String password,
+
+        @NotBlank
+        @Size(max = 100)
+        String firstName,
+
+        @NotBlank
+        @Size(max = 100)
+        String lastName
+) {
 }
