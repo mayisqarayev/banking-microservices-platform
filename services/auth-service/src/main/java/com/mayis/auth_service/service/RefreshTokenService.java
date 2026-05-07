7a91ef17573f16cbd5ec160e137492c282093fb2
Mayis Qarayev <mayisqarayev75@gmail.com>
2026-05-07 12:48:54 +0400

    feat(token): add refresh token service implementation

--- services/auth-service/src/main/java/com/mayis/auth_service/service/RefreshTokenService.java
+++ services/auth-service/src/main/java/com/mayis/auth_service/service/RefreshTokenService.java
@@ -1,4 +1,41 @@
 package com.mayis.auth_service.service;
 
+import com.mayis.auth_service.dto.GenerateRefreshTokenRequestDto;
+import com.mayis.auth_service.model.entity.RefreshToken;
+import com.mayis.auth_service.model.entity.User;
+import com.mayis.auth_service.repository.RefreshTokenRepository;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+
+@Service
 public class RefreshTokenService {
+
+    private final RefreshTokenRepository repository;
+    private final UserService userService;
+    private final PasswordEncoder passwordEncoder;
+
+    public RefreshTokenService(RefreshTokenRepository repository, UserService userService, PasswordEncoder passwordEncoder) {
+        this.repository = repository;
+        this.userService = userService;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+
+    public String generateRefreshToken(GenerateRefreshTokenRequestDto requestDto) {
+        User user = userService.getUserById(requestDto.userId());
+        String rawToken = UUID.randomUUID().toString();
+        RefreshToken token = new RefreshToken();
+
+        token.setUser(user);
+        token.setTokenHash(passwordEncoder.encode(rawToken));
+        token.setExpiresAt(LocalDateTime.now().plusDays(7));
+        token.setRevoked(false);
+
+        repository.save(token);
+        return rawToken;
+    }
 }
