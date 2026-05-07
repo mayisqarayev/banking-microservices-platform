7f8487f7a8388a38d4b25f7fdd649e2652e03631
Mayis Qarayev <mayisqarayev75@gmail.com>
2026-05-07 15:22:23 +0400

    chore(auth): enable auth security properties

--- services/auth-service/src/main/java/com/mayis/auth_service/AuthServiceApplication.java
+++ services/auth-service/src/main/java/com/mayis/auth_service/AuthServiceApplication.java
@@ -1,13 +1,16 @@
 package com.mayis.auth_service;
 
+import com.mayis.auth_service.config.properties.AuthSecurityProperties;
 import com.mayis.auth_service.config.properties.JwtProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
-@EnableConfigurationProperties(JwtProperties.class)
-public class AuthServiceApplication {
+@EnableConfigurationProperties({
+		JwtProperties.class,
+		AuthSecurityProperties.class
+})public class AuthServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AuthServiceApplication.class, args);
