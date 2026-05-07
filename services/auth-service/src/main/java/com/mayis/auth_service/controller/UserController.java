4d814cbb78636e86dd6ac0605a3a12dcc75897d1
Mayis Qarayev <mayisqarayev75@gmail.com>
2026-05-07 15:22:26 +0400

    feat(auth): expose user account lock endpoints

--- services/auth-service/src/main/java/com/mayis/auth_service/controller/UserController.java
+++ services/auth-service/src/main/java/com/mayis/auth_service/controller/UserController.java
@@ -1,13 +1,14 @@
 package com.mayis.auth_service.controller;
 
 import com.mayis.auth_service.dto.UserResponseDto;
-import com.mayis.auth_service.model.entity.User;
 import com.mayis.auth_service.service.UserService;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/api/v1/user")
@@ -22,5 +23,10 @@ public class UserController {
     @GetMapping
     public List<UserResponseDto> getAll() {
         return userService.getAll();
+    }
+
+    @GetMapping("/{id}")
+    public UserResponseDto getCurrentUserById(@PathVariable UUID id) {
+        return userService.getCurrentUser(id);
     }
 }
