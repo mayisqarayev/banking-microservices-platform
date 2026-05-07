f6d040a8960f21d979f9c0de1a3aaca474e8c924
Mayis Qarayev <mayisqarayev75@gmail.com>
2026-05-07 12:49:00 +0400

    feat(role): implement role management service

--- services/auth-service/src/main/java/com/mayis/auth_service/service/RoleService.java
+++ services/auth-service/src/main/java/com/mayis/auth_service/service/RoleService.java
@@ -1,4 +1,29 @@
 package com.mayis.auth_service.service;
 
+import com.mayis.auth_service.exception.RoleNotFoundException;
+import com.mayis.auth_service.model.entity.Role;
+import com.mayis.auth_service.model.enums.RoleName;
+import com.mayis.auth_service.repository.RoleRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+
+@Service
 public class RoleService {
+
+    private final RoleRepository roleRepository;
+
+    public RoleService(RoleRepository roleRepository) {
+        this.roleRepository = roleRepository;
+    }
+
+    protected Role getRoleByName(RoleName roleName) {
+        return roleRepository.findByName(roleName)
+                .orElseThrow(() -> new RoleNotFoundException("Role not found"));
+    }
+
+    protected Role getRoleById(UUID id) {
+        return roleRepository.findById(id)
+                .orElseThrow(() -> new RoleNotFoundException("Role not found"));
+    }
 }
