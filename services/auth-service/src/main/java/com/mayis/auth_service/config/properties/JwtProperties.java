4f9ec531de443ea2bf0dc2493ecc4fc6a1bdbbc7
Mayis Qarayev <mayisqarayev75@gmail.com>
2026-05-07 10:50:04 +0400

    feat(auth-service): add jwt properties configuration

--- services/auth-service/src/main/java/com/mayis/auth_service/config/properties/JwtProperties.java
+++ services/auth-service/src/main/java/com/mayis/auth_service/config/properties/JwtProperties.java
@@ -1,4 +1,16 @@
 package com.mayis.auth_service.config.properties;
 
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@Getter
+@Setter
+@ConfigurationProperties(prefix = "jwt")
 public class JwtProperties {
+
+    private String secret;
+    private long accessTokenExpiration;
+    private long refreshTokenExpiration;
+
 }
