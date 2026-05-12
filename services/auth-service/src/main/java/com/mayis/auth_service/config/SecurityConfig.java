391affff7cc5665e4f3800c435c37f970b44eadf
Mayis Qarayev <mayisqarayev75@gmail.com>
2026-05-12 16:08:18 +0400

    fix(auth-service): enable method security

--- services/auth-service/src/main/java/com/mayis/auth_service/config/SecurityConfig.java
+++ services/auth-service/src/main/java/com/mayis/auth_service/config/SecurityConfig.java
@@ -4,6 +4,7 @@ import com.mayis.auth_service.security.JwtAuthFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -14,6 +15,7 @@ import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
+@EnableMethodSecurity
 public class SecurityConfig {
 
     private final JwtAuthFilter jwtAuthFilter;
