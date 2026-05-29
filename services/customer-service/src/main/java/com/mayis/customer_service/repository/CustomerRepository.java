15e5fd16fe374dfff5bb8fcaf3e4ec31fb128104
Mayis Qarayev <mayisqarayev75@gmail.com>
2026-05-30 00:13:08 +0400

    feat(customer-service): add customer repository layer

--- services/customer-service/src/main/java/com/mayis/customer_service/repository/CustomerRepository.java
+++ services/customer-service/src/main/java/com/mayis/customer_service/repository/CustomerRepository.java
@@ -2,10 +2,12 @@ package com.mayis.customer_service.repository;
 
 import com.mayis.customer_service.model.entity.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 import java.util.UUID;
 
+@Repository
 public interface CustomerRepository extends JpaRepository<Customer, UUID> {
 
     boolean existsByUserId(UUID userId);
