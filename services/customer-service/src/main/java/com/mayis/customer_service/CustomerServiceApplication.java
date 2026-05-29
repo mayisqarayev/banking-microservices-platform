e6ebbcf38c3450a9de607e1a2f226ce58b9cec56
Mayis Qarayev <mayisqarayev75@gmail.com>
2026-05-30 00:13:02 +0400

    feat(customer-service): enable customer event consumer configuration

--- services/customer-service/src/main/java/com/mayis/customer_service/CustomerServiceApplication.java
+++ services/customer-service/src/main/java/com/mayis/customer_service/CustomerServiceApplication.java
@@ -1,9 +1,12 @@
 package com.mayis.customer_service;
 
+import com.mayis.customer_service.config.properties.KafkaTopicProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties(KafkaTopicProperties.class)
 public class CustomerServiceApplication {
 
 	public static void main(String[] args) {
