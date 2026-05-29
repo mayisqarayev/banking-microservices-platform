afa41536c91d3422aa57ec92bc1d01180b56ba44
Mayis Qarayev <mayisqarayev75@gmail.com>
2026-05-30 00:13:06 +0400

    feat(customer-service): add customer document persistence support

--- services/customer-service/src/main/java/com/mayis/customer_service/repository/CustomerDocumentRepository.java
+++ services/customer-service/src/main/java/com/mayis/customer_service/repository/CustomerDocumentRepository.java
@@ -2,8 +2,10 @@ package com.mayis.customer_service.repository;
 
 import com.mayis.customer_service.model.entity.CustomerDocument;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 
+@Repository
 public interface CustomerDocumentRepository extends JpaRepository<CustomerDocument, UUID> {
 }
