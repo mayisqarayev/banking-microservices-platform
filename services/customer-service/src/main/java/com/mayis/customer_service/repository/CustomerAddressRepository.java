fbfdf8da46c4c8ee6bea3320aa9cb19e83bd5101
Mayis Qarayev <mayisqarayev75@gmail.com>
2026-05-30 00:13:04 +0400

    feat(customer-service): add customer address persistence support

--- services/customer-service/src/main/java/com/mayis/customer_service/repository/CustomerAddressRepository.java
+++ services/customer-service/src/main/java/com/mayis/customer_service/repository/CustomerAddressRepository.java
@@ -2,8 +2,10 @@ package com.mayis.customer_service.repository;
 
 import com.mayis.customer_service.model.entity.CustomerAddress;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 
+@Repository
 public interface CustomerAddressRepository extends JpaRepository<CustomerAddress, UUID> {
 }
