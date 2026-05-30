11c11c79f9b53fa5b1a68653b4d2e82094c670cb
Mayis Qarayev <mayisqarayev75@gmail.com>
2026-05-31 00:00:30 +0400

    feat(account-service): integrate customer account creation flow

--- services/account-service/src/main/java/com/mayis/account_service/AccountServiceApplication.java
+++ services/account-service/src/main/java/com/mayis/account_service/AccountServiceApplication.java
@@ -1,9 +1,12 @@
 package com.mayis.account_service;
 
+import com.mayis.account_service.config.properties.KafkaTopicProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties(KafkaTopicProperties.class)
 public class AccountServiceApplication {
 
 	public static void main(String[] args) {
