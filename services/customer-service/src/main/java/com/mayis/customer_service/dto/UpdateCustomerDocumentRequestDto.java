18e50dd81b03d2d335e1cd96503043d6638ac3f8
Mayis Qarayev <mayisqarayev75@gmail.com>
2026-05-31 01:06:26 +0400

    feat(customer-document): add update document request dto

---
+++ services/customer-service/src/main/java/com/mayis/customer_service/dto/UpdateCustomerDocumentRequestDto.java
@@ -0,0 +1,17 @@
+package com.mayis.customer_service.dto;
+
+import com.mayis.customer_service.model.enums.DocumentType;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+import java.time.LocalDate;
+
+public record UpdateCustomerDocumentRequestDto(
+        @NotNull DocumentType documentType,
+        @NotBlank @Size(max = 100) String documentNumber,
+        @NotBlank @Size(max = 100) String issuingCountry,
+        LocalDate issueDate,
+        LocalDate expiryDate
+) {
+}
