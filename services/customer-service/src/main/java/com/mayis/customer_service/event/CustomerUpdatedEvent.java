d7e62c8378b0ef97be9b7cf2f8bedc8f4e2186b3
Mayis Qarayev <mayisqarayev75@gmail.com>
2026-05-31 01:14:22 +0400

    feat(customer-events): add customer updated event

---
+++ services/customer-service/src/main/java/com/mayis/customer_service/event/CustomerUpdatedEvent.java
@@ -0,0 +1,17 @@
+package com.mayis.customer_service.event;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+public record CustomerUpdatedEvent(
+        UUID eventId,
+        LocalDateTime occurredAt,
+        UUID customerId,
+        UUID userId,
+        String cif,
+        String firstName,
+        String lastName,
+        String email,
+        String status
+) {
+}
