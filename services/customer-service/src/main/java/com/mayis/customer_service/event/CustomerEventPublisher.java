47688c254861b2bebc887a7f8f0e318aadbc66f3
Mayis Qarayev <mayisqarayev75@gmail.com>
2026-05-31 01:14:12 +0400

    feat(customer-events): update customer event publisher contract

--- services/customer-service/src/main/java/com/mayis/customer_service/event/CustomerEventPublisher.java
+++ services/customer-service/src/main/java/com/mayis/customer_service/event/CustomerEventPublisher.java
@@ -3,4 +3,8 @@ package com.mayis.customer_service.event;
 public interface CustomerEventPublisher {
 
     void publishCustomerCreated(CustomerCreatedEvent event);
+
+    void publishCustomerUpdated(CustomerUpdatedEvent event);
+
+    void publishCustomerBlocked(CustomerBlockedEvent event);
 }
